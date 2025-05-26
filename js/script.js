d9e42e9fdd9c054a9da4359a4018f05002de1637
Emerson Conquista <emerson.conquista71@gmail.com>
2025-05-26 07:18:15 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("meuBotao");
+const mensagem = document.getElementById("mensagem");
+
+botao.onclick = function() {
+  mensagem.innerHTML = "Nunca pare de estudar e se reinventar! 🚀";
+}
